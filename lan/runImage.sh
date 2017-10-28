186513247ba259e57cfbf7706f5974a2a879c37b
brekol <brekol@gmail.com>
2017-10-28 18:54:39 +0200

    zmiana folderu

--- lan/runImage.sh
+++ lan/runImage.sh
@@ -1,9 +1,10 @@
 WAR_DIRECTORY=/home/brekol/workspaces/private/jbutils/jbutils-ui/target
-
+WAR_DIRECTORY2=/home/brekol/workspaces/private/jbcity-courses/jbcity-courses-ui/target
 
 docker run -d \
 --name jbutils \
 -p 8080:8080 \
 -p 80:80 \
 -v $WAR_DIRECTORY/jbutils.war:/usr/local/tomcat/webapps/jbutils.war \
+-v $WAR_DIRECTORY2/jbcity-courses.war:/usr/local/tomcat/webapps/jbcity-courses.war \
 jbutils
